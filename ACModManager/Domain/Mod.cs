88c1a9c0707e1400a70f8c372e17511632ebdad8
KevinMertens <kevin_mertens@telenet.be>
2015-02-05 19:24:26 +0100

    Started domain

---
+++ ACModManager/Domain/Mod.cs
@@ -0,0 +1,19 @@
+﻿namespace ACModManager.Domain
+{
+    class Mod
+    {
+        public string Name { get; set; }
+        public ModTypes Type { get; set; }
+        public string Version { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+
+    enum ModTypes
+    {
+        Track,
+        Car,
+        Skin,
+        App
+    }
+}
